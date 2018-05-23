cc3ff9af9ce8ece102f3c2cbcd8883b5d94cb70a
a350844866 <350844866@qq.com>
2018-05-23 09:49:38 +0800

    去重

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,10 +1,19 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
-
-        System.out.println("测试分支，最终版本");
+        List<String> list = new ArrayList();
+        list.add(null);
+        switch (list.get(0)) {
+            case "1":
+                break;
+            default:
+                break;
+        }
 
     }
 }
